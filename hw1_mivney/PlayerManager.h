78864c2872dadd53b30a42a2bd4f6c846f621a26
IvriHikri <94908435+IvriHikri@users.noreply.github.com>
2021-11-29 10:25:26 +0200

    player manager H file updated

--- hw1_mivney/PlayerManager.h
+++ hw1_mivney/PlayerManager.h
@@ -1,8 +1,25 @@
-//
-// Created by evrih on 27/11/2021.
-//
-
 #ifndef HW1_MIVNEY_PLAYERMANAGER_H
 #define HW1_MIVNEY_PLAYERMANAGER_H
 
+#include "AVLTree.h"
+#include "memory"
+using std::shared_ptr;
+using std::make_shared;
+class Player{
+    int level;
+    int player_id;
+};
+class Group{
+    int group_id;
+    AVLTree<shared_ptr<Player>> players_id_tree;
+    AVLTree<shared_ptr<Player>> players_level_tree;
+};
+class PlayerManager{
+    AVLTree<shared_ptr<Player>> all_players_id_tree;
+    AVLTree<shared_ptr<Player>> all_players_level_tree;
+    AVLTree<shared_ptr<Group>> all_groups_tree;
+    AVLTree<shared_ptr<Group>> not_empty_groups_tree;
+public:
+    PlayerManager();
+};
 #endif //HW1_MIVNEY_PLAYERMANAGER_H
